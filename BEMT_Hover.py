54b70194de964c8f55632348fb3bb329c0fc48fc
francescogervasio <fran.gervasio@studenti.unina.it>
2024-05-22 19:53:29 +0200

    Update BEMT_Hover
    Elimination of the T_c output. The function will give as output only the T_c corrected with the Prandtl correction function. All the outputs that not uses F_Prandtl have been eliminated. phi is calculated using the arctan function on the argument mu / r_bar + lambda_i / r_bar. In the Prandtl correction function F_Prandtl np.sin(phi[-1]) has been replaced by np.tan(phi[-1]).  Moreover, the for cycle has been eliminated and the overall process has been vectorized. Also, a command print() has been eliminated.

--- BEMT_Hover.py
+++ BEMT_Hover.py
@@ -1,15 +1,12 @@
 import numpy as np
-import math as ma
-from scipy.constants import g
 
 def BEMT(Helicopter, V_infty):
 
-    """BEMT is a method that executes the Blade Element Momentum Theory. It accepts as input some data of the
-    helicopter's rotor, specifically:
+    """BEMT_Hover is a method that executes the Blade Element Momentum Theory with respect for an helicopter in hover or
+        (or slow climbing) condition. It accepts as input some data of the helicopter's rotor and flight conditions,
+        specifically:
 
-    Altitude: the altitude at which the helicopter is hovering (m)
     V_infty: the helicopter horizontal fligth speed (m/s)
-    MTOW: Maximum Take-Off Weight of the helicopter (kg);
     R_mr: main rotor radius (m);
     R_hub: rotor hub radius(m);
     c_mr: chord of the main rotor's blade (m);
@@ -22,13 +19,13 @@ def BEMT(Helicopter, V_infty):
     It gives as output:
 
     Tc: Thrust coefficient;
-    Qc: Torque (Power) coefficient;
-    Tc_PR: Thrust coefficient (considering the Prandtl correction function);
-    Qc_PR: Torque (Power) coefficient (considering the Prandtl correction function);
     Pc_i: Induced Power coefficient;
     Pc_0: Parasite Power coefficient;
+    Qc: Torque (Power) coefficient;
+    dTcdr_bar: Incremental Thrust coefficient along the main rotor radius considering the Prandtl correction function;
+    dPcdr_bar: Incremental Power coefficient along the main rotor radius;
     FM: Figure of Merit;
-    FM_PR: Figure of Merit (considering the Prandtl correction function);
+    F_Prandtl: Prandtl correction function.
     """
 
     ## INPUT
@@ -41,96 +38,62 @@ def BEMT(Helicopter, V_infty):
     theta0 = Helicopter.theta0
     theta_tw = Helicopter.theta_tw
     Cla = Helicopter.Cla
-    Cd0 = (Helicopter.Cd0)
+    Cd0 = Helicopter.Cd0
     mu = V_infty/Omega_R_mr  # Rotor advance ratio
 
 
     ## GEOMETRIC PROPERTIES
 
-    sol = (N_mr*c_mr)/(R_mr*np.pi) # Rotor solidity
+    sol = (N_mr*c_mr)/(R_mr*np.pi)  # Rotor solidity
     r = np.linspace(R_hub, R_mr, num=19)  # Evenly spaced stations along the main rotor
     r_bar = r/R_mr  # Stations along the rotor adimensionalized with respect to the rotor radius R
 
     theta0 = np.deg2rad(theta0)  # Pitch angle at the root of the rotor expressed in rad
-    theta_tw =np.deg2rad(theta_tw)  # Blade twist (tip minus root incidence) expressed in rad
+    theta_tw = np.deg2rad(theta_tw)  # Blade twist (tip minus root incidence) expressed in rad
     theta = theta0 + theta_tw*r_bar  # Pitch angle distribution along the rotor (°) (supposed linear)
 
-    ## PHYSICAL  QUANTITY INIZIALIZATION
 
-    F_prandtl = np.zeros(len(r_bar))  # Prandtl function value along the main rotor
-    lambda_i = np.zeros(len(r_bar))  # Rotor induced inflow ratio along the main rotor
-    phi = np.zeros(len(r_bar))  # Angle of inflow along the main rotor  (rad)
-    alpha = np.zeros(len(r_bar))  # Angle of attach along the main rotor (rad)
-    Cl = np.zeros(len(r_bar))  # Lift coefficient along the main rotor
-    Cd = np.zeros(len(r_bar))  # Drag coefficient along the main rotor
-    dTcdr_bar = np.zeros(len(r_bar))  # Incremental Thrust coefficient along the main rotor
-    dTcdr_bar_PR = np.zeros(len(r_bar))  # Incremental Thrust coefficient along the main rotor
-                                         # considering the Prandtl correction function
-    dQcdr_bar = np.zeros(len(r_bar))  # Incremental Torque coefficient along the main rotor
-    dQcdr_bar_PR = np.zeros(len(r_bar))  # Incremental Torque coefficient along the main rotor
-                                         # considering the Prandtl correction function
-    dPc_i = np.zeros(len(r_bar))  # Incremental induced Power coefficient of the rotor element
-    dPc_0 = np.zeros(len(r_bar))  # Incremental parasite Power coefficient of the rotor element
+## DETERMINATION OF THE DISTRIBUTIONS OF ANGULAR AND AERODYNAMIC QUANTITY
 
+# The second degree equation for the induced inflow ratio has coefficient 1, C1 and C2
 
-## FOR CYCLE ALONG THE ROTOR
+    C1 = mu + Cla * sol / 8  # First degree coefficient
+    C2 = -r_bar * Cla * sol / 8 * (theta - mu / r_bar)  # Free term
 
-    for j in range(len(r_bar)):
+    lambda_i = (-C1 + np.sqrt(C1 ** 2 - 4 * C2)) / 2  # Induced inflow ratio
 
-     # The second degree equation for the induced inflow ratio has coefficient 1, C1 and C2
+    phi = np.arctan(mu / r_bar + lambda_i / r_bar)  # Inflow angle (rad)
+    alpha = theta - phi  # Angle of attack (rad)
 
-        C1 = mu + Cla * sol / 8  # First degree coefficient
-        C2 = -r_bar[j] * Cla * sol / 8 * (theta[j] - mu / r_bar[j])  # Free term
+    F_Prandtl = 2 / np.pi * np.arccos(np.exp(N_mr / (2 * np.tan(phi[-1])) * (r_bar - 1)))  # Prandtl correction
+                                                                                           # function
+    F_Prandtl[-1] = 0
 
-        lambda_i[j] = (-C1 + np.sqrt(C1**2 - 4 * C2)) / 2  # Induced inflow ratio
+    Cl = Cla * alpha  # Lift coefficient
+    Cd = Cd0  # Drag coefficient
 
-        phi[j] = mu / r_bar[j] + lambda_i[j] / r_bar[j]  # Inflow angle (rad)
-        alpha[j] = theta[j] - phi[j]  # Angle of attack (rad)
+    dTcdr_bar = 0.5 * sol * Cl * r_bar ** 2  # Incremental Thrust coefficient along the main rotor radius
 
-        F_prandtl[j] = 2 /np.pi * np.arccos(np.exp(N_mr / (2 * lambda_i[j]) * (r_bar[j] - 1)))  # Prandtl correction
-                                                                                                 # function
-        if r_bar[j] == 1:
-            F_prandtl[j] = 0
+    dTcdr_bar_PR = dTcdr_bar * F_Prandtl  # Incremental Thrust coefficient along the main rotor radius
+                                          # considering the Prandtl correction function
 
-        Cl[j] = Cla * alpha[j]  # Lift coefficient
-        Cd[j] = Cd0  # Drag coefficient
+    dPc_i = 0.5 * sol * Cl * phi * r_bar ** 3  # Incremental induced Power coefficient of the rotor along the main rotor
+                                               # radius
+    dPc_0 = 0.5 * sol * Cd * r_bar ** 3  # Incremental parasite Power coefficient along the main rotor radius
 
-
-        dTcdr_bar[j] = 0.5 * sol * Cl[j] * r_bar[j]**2  # Incremental Thrust coefficient along the main rotor
-
-        dTcdr_bar_PR[j] = dTcdr_bar[j] * F_prandtl[j]   # Incremental Thrust coefficient along the main rotor
-                                                        # considering the Prandtl correction function
-
-        print(dTcdr_bar_PR[j])
-        dQcdr_bar[j] = 0.5 * sol * Cd[j] * r_bar[j]**3 + lambda_i[j] * dTcdr_bar[j]  # Incremental Torque coefficient
-                                                                                     # along the main rotor
-
-        dQcdr_bar_PR[j] = 0.5 * sol * Cd[j] * r_bar[j]**3 + lambda_i[j] * dTcdr_bar_PR[j]  # Incremental Torque
-                                                                                           # coefficient along the main
-                                                                                           # rotor considering the
-                                                                                           # Prandtl correction function
-
-        dPc_i[j] = 0.5 * sol * Cl[j] * phi[j] * r_bar[j]**3  # Incremental induced Power coefficient of the rotor
-                                                             # element
-        dPc_0[j] = 0.5 * sol * Cd[j] * r_bar[j]**3  # Incremental parasite Power coefficient of the rotor element
-
+    dPcdr_bar = dPc_i + dPc_0 # Incremental Power coefficient along the main rotor radius
 
 # THRUST, TORQUE, POWER COEFFICIENTS AND FIGURE OF MERIT
 
-    Tc = np.round(np.trapz(dTcdr_bar, r_bar), 4) # Thrust coefficient
-    Tc_PR = np.round(np.trapz(dTcdr_bar_PR, r_bar),4)  # Thrust coefficient  considering the Prandtl correction
-                                                               # function
-
-    Qc = np.round(np.trapz(dQcdr_bar, r_bar),8)  # Torque (Power) coefficient
-    Qc_PR = np.round(np.trapz(dQcdr_bar_PR, r_bar),8)  # Torque (Power) coefficient considering the Prandtl
-                                                               # correction function
+    Tc_PR = np.round(np.trapz(dTcdr_bar_PR, r_bar),8)  # Thrust coefficient  considering the Prandtl correction
+                                                                # function
 
     Pc_i = np.round(np.trapz(dPc_i, r_bar),8) # Induced Power (Torque) coefficient
     Pc_0 = np.round(np.trapz(dPc_0, r_bar),8) # Parasite Power (Torque) coefficient
 
-    FM = ((Tc**(3/2) / np.sqrt(2)))/Qc # Figure of Merit
-    FM_PR = ((Tc_PR ** (3 / 2) / np.sqrt(2))) / Qc_PR  # Figure of Merit considering the Prandtl correction function
+    Qc = np.round(np.trapz(dPcdr_bar, r_bar),8)  # Torque (Power) coefficient
 
+    FM = ((Tc_PR**(3/2)/np.sqrt(2)))/Qc  # Figure of Merit
 
-    return Tc, Qc, Tc_PR, Qc_PR, Pc_i, Pc_0, FM, FM_PR
+    return Tc_PR, Pc_i, Pc_0, Qc, dTcdr_bar_PR, dPcdr_bar, FM, F_Prandtl
 
