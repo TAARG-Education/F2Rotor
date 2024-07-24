b3b8368d9ae6228922b9072e7bbddcdbd760e96c
Eduardo <ed.duraccio@studenti.unina.it>
2024-07-24 12:35:04 +0200

    Add files via upload

--- Helicopter_Class.py
+++ Helicopter_Class.py
@@ -1,24 +1,35 @@
-class Helicopter:
-    def __init__(self, Omega_r_mr, R_mr, R_hub, c_mr, N_mr, theta0, theta_tw, Cla, Cd0, Omega_r_tr, R_tr, R_hub_tr, c_tr, N_tr, l_tr):
+class Helicopter_HS:
+    def __init__(self, tau, b_w, AR, R_LS, Hf, r, gamma, k, 
+                 R_w_b, c_w, R_mr, ala_fissa, profilo, turbolentflow):
+        
 
-        # Main rotor
-        self.Omega_r_mr = Omega_r_mr # Main rotor tip speed (m/s)
-        self.R_mr = R_mr  # Main rotor radius (m)
-        self.R_hub = R_hub  # Rotor hub radius (m)
-        self.c_mr = c_mr  # Rotor's chord (m)
-        self.N_mr = N_mr  # Number of blades of the helicopter
-        self.theta0 = theta0  # Pitch angle at the root (°)
-        self.theta_tw = theta_tw  # Blade twist (tip minus root incidence) (°)
-        self.Cla = Cla  # Lift curve slope of the airfoil (1/rad)
-        self.Cd0 = Cd0  # Profile drag coefficient of the airfoil
+        self.tau = tau
+        self.b_w = b_w
+        self.AR = AR
+        self.R_LS = R_LS
+        self. Hf = Hf
+        self.r = r
+        self.gamma = gamma
+        self.k = k
+        self.R_w_b = R_w_b
+        self.c_w=c_w
+        self.R_mr=R_mr
+        self.M_inf = None
+        self.Re_inf = None
+        self.ala_fissa = ala_fissa
+        self.profilo = profilo
+        self.turbolentflow = turbolentflow
 
-        # Tail rotor
-        self.Omega_r_tr = Omega_r_tr         # Tail rotor tip speed (m/s)
-        self.R_tr = R_tr                     # Tail rotor radius (m)
-        self.R_hub_tr = R_hub_tr             # Tail Rotor hub radius (m)
-        self.c_tr = c_tr                     # Tail rotor chord (m)
-        self.N_tr = N_tr                     # Number of blades of tail rotor
-        self.l_tr = l_tr                     # Distance between tail rotor and main rotor
+        
 
+    def set_Re_inf(self, v, rho, nu):
+        self.Re_inf = (v * self.c_w) / nu
 
-        # Developing the possibility of integration within the feature_geometry
+    def set_M_inf(self, v, a):
+        self.M_inf = v/a
+        
+
+   
+        
+        
+
