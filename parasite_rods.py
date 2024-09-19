2b488ec19b04e6fe428c781c340303c905cb111e
CatelloDelSorbo <catello.del.sorbo@outlook.it>
2024-09-19 16:15:25 +0200

    Aggiornamento versione codice

--- parasite_rods.py
+++ parasite_rods.py
@@ -1,44 +1,78 @@
+#PARASITE RODS
+#Description: This function calculates the parasite drag area
+#             of the landing gear rods, assuming it is a circular section cylinder
+#
+#   WARNING: This code operates on a helicopter configuration that uses TWO rods
+#            for the FRONT landing gear and ONE for the REAR landing gear.
+#            If the helicopter you are working with does not follow this configuration,
+#            it is necessary to modify the code internally by multiplying the values
+#            of f_A and f_A_lr by the correct number of rods.
+#
+#References:-"Lezioni Aerodinamica dell'ala rotante", v2.05, Renato Tognaccini,
+#             section 7.5, "Potenza parassita della fusoliera"
+#           -"Fluid-Dynamic Drag", Hoerner, chapter 3, section 5, "Drag of round bodies"
+#
+#Author: Catello Del Sorbo, Marcello Zammarrelli
+#Rotary Wing Aerodynamics course, prof. Renato Tognaccini
+#University of Naples Federico II
+#Academic Year 2023-2024
+#Date:17/09/2024
+#
+#Version:1.0.0
+
+
+import math as ma
+
 def landing_rods(self):
-    '''Methods that evaluates the parasite drag area of the landing rods'''
+    '''Methods that evaluates the parasite drag area of the landing rods.
 
-    # Extracting the parameters
-    R_b = self.config["ParasiteArea"][4]["R"]
-    L_b = self.config["ParasiteArea"][4]["L"]
-    alpha = self.config["ParasiteArea"][4]["alpha"]
-    S = self.S
+    Input:
+        -R:     rod radius
+        -L:     rod length
+        -alpha: angle of attack [rad]
+        -S:     reference area
 
-    # Calculating the wet Area
-    # the wet area corresponds to the lateral area of a cylinder
-    S_wet = ma.pi*R_b*L_b
-    # Calculating the drag coefficient
-    # this is an empirical formula. 1.1 is the basic drag coefficient of the cylinder
-    # at alpha=90 degrees
-    CD = 1.1*(ma.sin(alpha))**3+0.02
-    # Calculating the adimensional parasite drag area of the hub
-    f_A_lr_b = CD*S_wet/S
-    # Calculating the parasite drag area of the hub
-    # alternatively we can see this formula as:
-    # f_cs= CD*S_wet
-    f_lr_b = f_A_lr_b*S
-    # Rounding of f_Acs and f_cs
-    f_A_lr_b = round(f_A_lr_b,5)
-    f_lr_b = round(f_lr_b,4)
+    Output:
+        f_A_lr: Combined parasite drag area of the landing rods.
+        f_lr: Total parasite drag of the landing rods.
+    '''
 
-    R_f = self.config["ParasiteArea"][5]["R"]
-    L_f = self.config["ParasiteArea"][5]["L"]
-    alpha = self.config["ParasiteArea"][5]["alpha"]
-    S = self.config["ParasiteArea"][5]["S"]
+    def calculate_drag_for_section(section_index):
+        ''' Helper function to calculate drag for a given section of the landing rods '''
+        R = self.config["ParasiteArea"][section_index]["R"]
+        L = self.config["ParasiteArea"][section_index]["L"]
+        alpha = self.config["ParasiteArea"][section_index]["alpha"]
+        S = self.config["ParasiteArea"][section_index]["S"]
 
-    S_wet = ma.pi*R_f*L_f
-    CD = 1.1*(ma.sin(alpha))**3+0.02
+        # Calculating the wet area
+        # The wet area corresponds to the lateral area of a cylinder
+        S_wet = ma.pi * R * L
 
-    f_A_lr_f = CD*S_wet/S
-    f_lr_f = f_A_lr_f*S
+        # Calculating the drag coefficient
+        # This is an empirical formula from "Fluid-Dynamic Drag", Hoerner,
+        # chapter 3, section 5, "Drag of round bodies"
+        CD = 1.1 * (ma.sin(alpha)) ** 3 + 0.02
 
-    f_A_lr_f = 2*round(f_A_lr_f,5)
-    f_lr_f = 2*round(f_lr_f,4)
+        # Calculating the adimensional parasite drag area of the hub
+        f_A_lr = CD * S_wet / S
+        f_lr = f_A_lr * S
 
+        # Rounding of f_Acs and f_cs
+        return round(f_A_lr, 5), round(f_lr, 4)
+
+
+    # Calculate for the back section (index 4)
+    f_A_lr_b, f_lr_b = calculate_drag_for_section(4)
+
+    # Calculate for the front section (index 5)
+    f_A_lr_f, f_lr_f = calculate_drag_for_section(5)
+
+    # Adjust front section for symmetry (x2)
+    f_A_lr_f *= 2
+    f_lr_f *= 2
+
+    # Combine both sections
     f_A_lr = f_A_lr_b + f_A_lr_f
-    f_lr = f_lr_b + 2*f_lr_f
-    # Results
+    f_lr = f_lr_b + f_lr_f
+
     return f_A_lr, f_lr
