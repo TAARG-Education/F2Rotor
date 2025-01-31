287729f509a62432011f9a78a36758a3bd967d78
GGiangreco <166157038+GGiangreco@users.noreply.github.com>
2025-01-31 15:47:39 +0100

    Update Mission_profile_testcase.py
    Resolved issues on helicopter class function

--- TestCases/Mission_profile_testcase.py
+++ TestCases/Mission_profile_testcase.py
@@ -17,11 +17,12 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt                     
-from matplotlib import cm                           
+from matplotlib import cm
+from matplotlib.table import Table                  
 from ambiance import Atmosphere                    
-from scipy.constants import pi, g                   
+from scipy.constants import pi, g
+from Helicopter_Class import Helicopter
 from Mission_profile import Mission_profile
-from Mission_profile import create_table_figure
 from Drone_functions import RigidFW2
 from Climb import Helicopter_properties
 from BEMT_Hover import BEMT_Hover
@@ -29,49 +30,39 @@ from Drone_functions import Calculate_alpha_hover
 
 # Baisc Input
 
-MTOW = 40000                                                                             # Maximum Take-Off Weight (N).
-W = 40000                                                                                         # Initial Weight (N).
-c_r = 0.45                                                                                            # Root chord [m].
-c_t = 0.35                                                                                             # Tip chord [m].
-r_segn = np.linspace(0.1, 1, 19)                                                      # Dimensionless disk rotor radius.
-c_tw = (c_t-c_r)/(r_segn[-1]-r_segn[0])                                                               # Twist chord [m].
-Vinf_vec = np.linspace(0, 65, 10)                                                          # Asymptotic air speed [m/s].
+MTOW = 40000                                                         # Maximum Take-Off Weight (N).
+W = 40000                                                            # Initial Weight (N).
+c_r = 0.45                                                           # Root chord [m].
+c_t = 0.35                                                           # Tip chord [m].
+r_segn = np.linspace(0.1, 1, 19)                                     # Dimensionless disk rotor radius.
+c_tw = (c_t-c_r)/(r_segn[-1]-r_segn[0])                              # Twist chord [m].
+Vinf_vec = np.linspace(0, 65, 10)                                    # Asymptotic air speed [m/s].
 
-# Input for Helicopter class.
-class Helicopter:
+Omega = 27                                                           # Main rotor angular velocity.
+R = 5.5                                                              # Disk rotor radius [m].
+R_hub = 0.1 * R                                                      # Hub rotor radius [m].
+c = c_r + c_tw*(r_segn-r_segn[0])                                    # Chord law [m].
+N = 4                                                                # Number of blades.
+theta0 = 25                                                          # Initial pitch angle [deg].
+theta_end = 15                                                       # Tip pitch angle [deg].
+theta_tw = (theta_end-theta0)/(r_segn[-1]-r_segn[0])                 # Pitch law [deg].
+Cla = 2 * pi                                                         # Lift slope curve [rad^-1].
+Cd0 = 0.012                                                          # Parasitic drag coefficient.
+Omega_r_tr = 80                                                      # Tail rotor tip speed (m/s)
+R_tr = 1.5                                                           # Tail rotor radius (m)
+R_hub_tr = 0.3                                                       # Tail rotor hub radius (m)
+c_tr = 0.25                                                          # Tail rotor chord length (m)
+N_tr = 2                                                             # Number of blades of the tail rotor
+l_tr = 3                                                             # Distance between the main rotor and tail rotor (m)
 
-    Omega_r_mr = 25                                                                       # Main rotor angular velocity.
-    R_mr = 5.5                                                                                  # Disk rotor radius [m].
-    R_hub = 0.1 * R_mr                                                                           # Hub rotor radius [m].
-    c_mr = c_r + c_tw*(r_segn-r_segn[0])                                                                # Chord law [m].
-    N_mr = 4                                                                                         # Number of blades.
-    theta0 = 25                                                                             # Initial pitch angle [deg].
-    theta_end = 15                                                                                # Tip pitch angle [deg].
-    theta_tw = (theta_end-theta0)/(r_segn[-1]-r_segn[0])                                              # Pitch law [deg].
-    Cla = 2 * pi                                                                            # Lift slope curve [rad^-1].
-    Cd0 = 0.012                                                                            # Parasitic drag coefficient.
-
-    # Class attributes.
-    def __init__(self, Omega_r_mr, R_mr, R_hub, c_mr, N_mr, theta0, theta_tw, Cla, Cd0):
-        Helicopter.Omega_r_mr = Omega_r_mr
-        Helicopter.R_mr = R_mr
-        Helicopter.R_hub = R_hub
-        Helicopter.c_mr = c_mr
-        Helicopter.N_mr = N_mr
-        Helicopter.theta0 = theta0
-        Helicopter.theta_tw = theta_tw
-        Helicopter.Cla = Cla
-        Helicopter.Cd0 = Cd0
+Helicopter = Helicopter(Omega, R, R_hub, c, N, theta0, theta_tw, Cla, Cd0, 
+                        Omega_r_tr, R_tr, R_hub_tr, c_tr, N_tr, l_tr)
 
 # Input variables
-
-R = Helicopter.R_mr
-N = Helicopter.N_mr
-c = Helicopter.c_mr
 SFC = 0.7                                       # Specific Fuel Consumption (kg/kWh)
 Pd = 500000 * 2                                 # Available power (W)
 height_c = 3298.78                              # Climb altitude (m)
-Voo = 20                                        # Operating speed (m/s)
+Voo = 20                                        # Speed during climb (m/s)
 Voo_d = 50                                      # Speed during descent (m/s)
 height_h = 451.10                               # Hover altitude (m)
 time_h = 120                                    # Hover time (s)
@@ -80,10 +71,6 @@ dist_f = 75200                                  # Forward flight distance (m)
 height_f = 3298.78                              # Forward flight altitude (m)
 A = math.pi * R**2                              # Rotor area
 f_A = 0.00001                                   # Friction coefficient
-Clalpha = 5.7                                   # Lift curve slope
-Cd_mean = 0.01                                  # Mean drag
-Omega = 27                                      # Rotor angular velocity (rad/s)
-N_rotors = 1                                    # Number of rotors
 F_Prandtl = np.full(19, 0.98)                   # Prandtl factor
 height_d = 451.1                                # Final altitude (m)
 height_f2 = 451.1                               # Second Forward flight altitude (m)
@@ -115,14 +102,78 @@ climb_data, hover_data, forward_data, Descend_data, forward_data2, fuel_data, ga
     R=R,
     A=A,
     f_A=f_A,
-    Clalpha=Clalpha,
-    Cd_mean=Cd_mean,
+    Clalpha=Cla,
+    Cd_mean=Cd0,
     Omega=Omega,
-    N_rotors=N_rotors,
+    N_rotors=N,
     F_Prandtl=F_Prandtl,
     height_d=height_d,
     height_f2=height_f2,
     dist_f2=dist_f2
 )
+
+# Graphics
+
+def create_table_figure(climb_data, hover_data, forward_data, Descend_data, forward_data2, fuel_data, gamma_deg, gamma_deg_d):
+    
+    headers = ["Segment", "Altitude \n(ft)", "Speed \n(knots - TAS)", "Distance \n(nm)", "Time \n(s)", 
+               "Power Rating \n(%)", "Fuel Flow \n(kg/h)", "Fuel Used \n(kg)", "Gross Weight \n(kg)"]
+    
+    rows = [
+        ["Warm-up", "-", "-", "-", "-", "-", "-", "-", "-"],
+        ["Climb", climb_data["Altitude"], climb_data["Speed "], climb_data["Distance"], climb_data["Time"], 
+         climb_data["Power Rating"], climb_data["Fuel Flow"], climb_data["Fuel Used"], climb_data["Gross Weight"]],
+        ["Cruise", forward_data["Altitude"], forward_data["Speed "], forward_data["Distance"], forward_data["Time"], 
+         forward_data["Power Rating"], forward_data["Fuel Flow"], forward_data["Fuel Used"], forward_data["Gross Weight"]],
+        ["Descend", Descend_data["Altitude"], Descend_data["Speed "], Descend_data["Distance"], Descend_data["Time"], 
+         Descend_data["Power Rating"], Descend_data["Fuel Flow"], Descend_data["Fuel Used"], Descend_data["Gross Weight"]],
+        ["Hover", hover_data["Altitude"], hover_data["Speed "], hover_data["Distance"], hover_data["Time"], 
+         hover_data["Power Rating"], hover_data["Fuel Flow"], hover_data["Fuel Used"], hover_data["Gross Weight"]],
+        ["Cruise", forward_data2["Altitude"], forward_data2["Speed "], forward_data2["Distance"], forward_data2["Time"], 
+         forward_data2["Power Rating"], forward_data2["Fuel Flow"], forward_data2["Fuel Used"], forward_data2["Gross Weight"]],
+        ["Final descent", "-", "-", "-", "-", "-", "-", "-", "-"]
+    ]
+    
+    fig, ax = plt.subplots(figsize=(12, 6)) 
+    ax.axis('tight')
+    ax.axis('off')
+    
+    # Table creation
+    table = Table(ax, bbox=[0, 0.2, 1, 0.7])  
+    n_cols = len(headers)
+    n_rows = len(rows) + 1  
+
+    header_fontsize = 15
+    cell_fontsize = 15
+    footer_fontsize = 10
+
+    for col in range(n_cols):
+        cell = table.add_cell(0, col, width=1/n_cols, height=0.1, text=headers[col], loc='center', facecolor='lightgrey')
+        cell.set_fontsize(header_fontsize)
+
+    for row_idx, row in enumerate(rows):
+        for col_idx, cell_value in enumerate(row):
+            if row_idx == 1 and col_idx == 1:  
+                cell_value_with_note = f"{str(cell_value)}\nγ = {round(gamma_deg)}°"
+            elif row_idx == 3 and col_idx == 1:
+                cell_value_with_note = f"{str(cell_value)}\nγ = - {round(gamma_deg_d)}°"
+
+            else:
+                cell_value_with_note = str(cell_value)
+        
+            cell = table.add_cell(row_idx + 1, col_idx, width=1/n_cols, height=0.1, text=str(cell_value), loc='center')
+            cell = table.add_cell(row_idx + 1, col_idx, width=1/n_cols, height=0.1, text=cell_value_with_note, loc='center')
+            cell.set_fontsize(cell_fontsize)
+
+    ax.add_table(table)
+
+    footer_text = (
+        "Take-off GW: " + str(fuel_data["MTOW"]) + " kg\n"
+        "Total fuel consumption: " + str(fuel_data["Total fuel Consuption"]) + " kg\n"
+        "Total flight time: " + str(round((climb_data["Time"] + forward_data["Time"] + hover_data["Time"] + Descend_data["Time"])/60)) + " minutes"
+    )
+    plt.figtext(0.5, 0.02, footer_text, wrap=True, horizontalalignment='center', fontsize=footer_fontsize)
+
+    plt.show()
 
 create_table_figure(climb_data, hover_data, forward_data, Descend_data, forward_data2, fuel_data, gamma_deg, gamma_deg_d)
