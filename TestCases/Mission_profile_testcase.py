aae4d66d21a2012edda915498c2546f1a6552a52
GGiangreco <166157038+GGiangreco@users.noreply.github.com>
2025-02-01 13:19:04 +0100

    Update Mission_profile_testcase.py

--- TestCases/Mission_profile_testcase.py
+++ TestCases/Mission_profile_testcase.py
@@ -30,30 +30,30 @@ from Drone_functions import Calculate_alpha_hover
 
 # Baisc Input
 
-MTOW = 40000                                                         # Maximum Take-Off Weight (N).
-W = 40000                                                            # Initial Weight (N).
-c_r = 0.45                                                           # Root chord [m].
-c_t = 0.35                                                           # Tip chord [m].
-r_segn = np.linspace(0.1, 1, 19)                                     # Dimensionless disk rotor radius.
-c_tw = (c_t-c_r)/(r_segn[-1]-r_segn[0])                              # Twist chord [m].
-Vinf_vec = np.linspace(0, 65, 10)                                    # Asymptotic air speed [m/s].
+MTOW = 40000                                                    # Maximum Take-Off Weight (N).
+W = 40000                                                       # Initial Weight (N).
+c_r = 0.45                                                      # Root chord [m].
+c_t = 0.35                                                      # Tip chord [m].
+r_segn = np.linspace(0.1, 1, 19)                                # Dimensionless disk rotor radius.
+c_tw = (c_t-c_r)/(r_segn[-1]-r_segn[0])                         # Twist chord [m].
+Vinf_vec = np.linspace(0, 65, 10)                               # Asymptotic air speed [m/s].
 
-Omega = 27                                                           # Main rotor angular velocity.
-R = 5.5                                                              # Disk rotor radius [m].
-R_hub = 0.1 * R                                                      # Hub rotor radius [m].
-c = c_r + c_tw*(r_segn-r_segn[0])                                    # Chord law [m].
-N = 4                                                                # Number of blades.
-theta0 = 25                                                          # Initial pitch angle [deg].
-theta_end = 15                                                       # Tip pitch angle [deg].
-theta_tw = (theta_end-theta0)/(r_segn[-1]-r_segn[0])                 # Pitch law [deg].
-Cla = 2 * pi                                                         # Lift slope curve [rad^-1].
-Cd0 = 0.012                                                          # Parasitic drag coefficient.
-Omega_r_tr = 80                                                      # Tail rotor tip speed (m/s)
-R_tr = 1.5                                                           # Tail rotor radius (m)
-R_hub_tr = 0.3                                                       # Tail rotor hub radius (m)
-c_tr = 0.25                                                          # Tail rotor chord length (m)
-N_tr = 2                                                             # Number of blades of the tail rotor
-l_tr = 3                                                             # Distance between the main rotor and tail rotor (m)
+Omega = 27                                                      # Main rotor angular velocity.
+R = 5.5                                                         # Disk rotor radius [m].
+R_hub = 0.1 * R                                                 # Hub rotor radius [m].
+c = c_r + c_tw*(r_segn-r_segn[0])                               # Chord law [m].
+N = 4                                                           # Number of blades.
+theta0 = 25                                                     # Initial pitch angle [deg].
+theta_end = 15                                                  # Tip pitch angle [deg].
+theta_tw = (theta_end-theta0)/(r_segn[-1]-r_segn[0])            # Pitch law [deg].
+Cla = 2 * pi                                                    # Lift slope curve [rad^-1].
+Cd0 = 0.012                                                     # Parasitic drag coefficient.
+Omega_r_tr = 80                                                 # Tail rotor tip speed (m/s)
+R_tr = 1.5                                                      # Tail rotor radius (m)
+R_hub_tr = 0.3                                                  # Tail rotor hub radius (m)
+c_tr = 0.25                                                     # Tail rotor chord length (m)
+N_tr = 2                                                        # Number of blades of the tail rotor
+l_tr = 3                                                        # Distance between the main rotor and tail rotor (m)
 
 Helicopter = Helicopter(Omega, R, R_hub, c, N, theta0, theta_tw, Cla, Cd0, 
                         Omega_r_tr, R_tr, R_hub_tr, c_tr, N_tr, l_tr)
@@ -118,19 +118,19 @@ def create_table_figure(climb_data, hover_data, forward_data, Descend_data, forw
     
     headers = ["Segment", "Altitude \n(ft)", "Speed \n(knots - TAS)", "Distance \n(nm)", "Time \n(s)", 
                "Power Rating \n(%)", "Fuel Flow \n(kg/h)", "Fuel Used \n(kg)", "Gross Weight \n(kg)"]
-    
+
     rows = [
         ["Warm-up", "-", "-", "-", "-", "-", "-", "-", "-"],
-        ["Climb", climb_data["Altitude"], climb_data["Speed "], climb_data["Distance"], climb_data["Time"], 
+        ["Climb", f"To {round(float(climb_data['Altitude']))}", climb_data["Speed "], climb_data["Distance"], climb_data["Time"], 
          climb_data["Power Rating"], climb_data["Fuel Flow"], climb_data["Fuel Used"], climb_data["Gross Weight"]],
-        ["Cruise", forward_data["Altitude"], forward_data["Speed "], forward_data["Distance"], forward_data["Time"], 
-         forward_data["Power Rating"], forward_data["Fuel Flow"], forward_data["Fuel Used"], forward_data["Gross Weight"]],
-        ["Descend", Descend_data["Altitude"], Descend_data["Speed "], Descend_data["Distance"], Descend_data["Time"], 
+        ["Cruise", forward_data['Altitude'], forward_data["Speed "], forward_data["Distance"], forward_data["Time"], 
+         f"{round(float(forward_data["Power Rating"]))}%", forward_data["Fuel Flow"], forward_data["Fuel Used"], forward_data["Gross Weight"]],
+        ["Descend",f"To {round(float(Descend_data['Altitude']))}", Descend_data["Speed "], Descend_data["Distance"], Descend_data["Time"], 
          Descend_data["Power Rating"], Descend_data["Fuel Flow"], Descend_data["Fuel Used"], Descend_data["Gross Weight"]],
         ["Hover", hover_data["Altitude"], hover_data["Speed "], hover_data["Distance"], hover_data["Time"], 
-         hover_data["Power Rating"], hover_data["Fuel Flow"], hover_data["Fuel Used"], hover_data["Gross Weight"]],
+         f"{round(float(hover_data["Power Rating"]))}%", hover_data["Fuel Flow"], hover_data["Fuel Used"], hover_data["Gross Weight"]],
         ["Cruise", forward_data2["Altitude"], forward_data2["Speed "], forward_data2["Distance"], forward_data2["Time"], 
-         forward_data2["Power Rating"], forward_data2["Fuel Flow"], forward_data2["Fuel Used"], forward_data2["Gross Weight"]],
+         f"{round(float(forward_data2["Power Rating"]))}%", forward_data2["Fuel Flow"], forward_data2["Fuel Used"], forward_data2["Gross Weight"]],
         ["Final descent", "-", "-", "-", "-", "-", "-", "-", "-"]
     ]
     
@@ -153,23 +153,35 @@ def create_table_figure(climb_data, hover_data, forward_data, Descend_data, forw
 
     for row_idx, row in enumerate(rows):
         for col_idx, cell_value in enumerate(row):
-            if row_idx == 1 and col_idx == 1:  
-                cell_value_with_note = f"{str(cell_value)}\nγ = {round(gamma_deg)}°"
-            elif row_idx == 3 and col_idx == 1:
-                cell_value_with_note = f"{str(cell_value)}\nγ = - {round(gamma_deg_d)}°"
+            # Converti solo i valori numerici in float e arrotondali a 1 decimale
+            try:
+                cell_value = round(float(cell_value), 1)
+            except (ValueError, TypeError):
+                pass  # Se non è un numero, lo lascia invariato
 
+            # Converte i valori in colonna 1 delle righe 3, 5, 6 in interi
+            if col_idx == 1 and row_idx in [2, 4, 5]:
+                try:
+                    cell_value = int(float(cell_value))  # Converte in intero
+                except (ValueError, TypeError):
+                    pass  # Se non è convertibile in intero, lo lascia invariato
+
+            if row_idx == 1 and col_idx == 1:
+                cell_value_with_note = f"{cell_value}\nγ = {round(gamma_deg)}°"
+            elif row_idx == 3 and col_idx == 1:
+                cell_value_with_note = f"{cell_value}\nγ = - {round(gamma_deg_d)}°"
             else:
                 cell_value_with_note = str(cell_value)
-        
-            cell = table.add_cell(row_idx + 1, col_idx, width=1/n_cols, height=0.1, text=str(cell_value), loc='center')
+
             cell = table.add_cell(row_idx + 1, col_idx, width=1/n_cols, height=0.1, text=cell_value_with_note, loc='center')
             cell.set_fontsize(cell_fontsize)
 
-    ax.add_table(table)
+
+        ax.add_table(table)
 
     footer_text = (
-        "Take-off GW: " + str(fuel_data["MTOW"]) + " kg\n"
-        "Total fuel consumption: " + str(fuel_data["Total fuel Consuption"]) + " kg\n"
+        "Take-off GW: " + str(round(fuel_data["MTOW"])) + " kg\n"
+        "Total fuel consumption: " + str(round(fuel_data["Total fuel Consuption"])) + " kg\n"
         "Total flight time: " + str(round((climb_data["Time"] + forward_data["Time"] + hover_data["Time"] + Descend_data["Time"])/60)) + " minutes"
     )
     plt.figtext(0.5, 0.02, footer_text, wrap=True, horizontalalignment='center', fontsize=footer_fontsize)
